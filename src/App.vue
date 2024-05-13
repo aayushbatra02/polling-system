0ef344ea94158ba0fcd6f33021663b81f88462e1
Aayush Batra <aayushbatra@innotechteam.in>
2024-05-13 15:02:01 +0530

    minor bug solve

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <nav-bar v-if="route.name !== 'Login' && route.name !== 'Signup'" />
+    <nav-bar v-if="user" />
     <router-view />
   </div>
 </template>
 
 <script setup>
-import { useRoute } from "vue-router";
 import NavBar from "./components/NavBar.vue";
-const route = useRoute();
+import { useAuthStore } from "./stores/authStore";
+import { storeToRefs } from "pinia";
+const authStore = useAuthStore();
+const { user } = storeToRefs(authStore);
 </script>
